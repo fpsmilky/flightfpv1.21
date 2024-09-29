2d4751d9f92e161e374c898d94b267065add5486
Thighs <fpsmilky@gmail.com>
2024-09-28 22:51:26 -0400

    added important comment

--- src/main/java/net/michael/flightfpv/FlightFPV.java
+++ src/main/java/net/michael/flightfpv/FlightFPV.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Very important comment
 public class FlightFPV implements ModInitializer {
 	public static final String MOD_ID = "flightfpv";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -14,4 +15,3 @@ public class FlightFPV implements ModInitializer {
 
 	}
 }
-//hello
